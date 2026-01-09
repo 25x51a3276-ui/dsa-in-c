f4b75d2eb827a9eb7f81522dfdb9b7fd671acf76
25x51a3276-ui <25x51a3276@srecnandyal.edu.in>
2026-01-09 17:52:04 +0530

    Array.c
    Code finds the number present in zero index and print it 
    Output:- 10 
    Because the index position is zero

---
+++ Array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Declaring and initializing an array
+    int numbers[] = {10, 20, 30, 40, 50};
+    
+    printf("First element in the array: %d\n", numbers[0]);
+    
+    return 0;
+}
+
